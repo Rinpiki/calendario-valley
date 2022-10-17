ae77e56eccf53ed0f81ee26e4a465b77af0cb71d
Marcos Pablo <rinpikibr@gmail.com>
2022-10-17 15:06:48 -0300

    bug de rotas resolvido

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import './App.css';
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
-import List from './components/List';
+import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className="bg-[#D9F4FF] flex flex-col w-[319px] h-[500px] max-w-4xl ">
-        <Header />
-        <Routes>
-          <Route path="/" element={<List />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className="bg-[#D9F4FF] flex flex-col w-[319px] h-[500px] max-w-4xl ">
+      <Header />
+    </main>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-function Header(props: any) {
-  return (
-    <Header>
-      <div>
-        <ul>
-          <li>
-            <Link to="/season1">mes1</Link>
-          </li>
-          <li>
-            <Link to="/season2">mes2</Link>
-          </li>
-          <li>
-            <Link to="/season3">mes3</Link>
-          </li>
-          <li>
-            <Link to="/season4">mes4</Link>
-          </li>
-        </ul>
-      </div>
-    </Header>
-  );
+function Header() {
+  return <div className="bg-slate-600">oi</div>;
 }
 export default Header;
